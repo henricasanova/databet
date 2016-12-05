b209d4ae5d01545ebc6fcb8c055e075429bcce23
henricasanova <henric@hawaii.edu>
2016-12-04 15:48:24 -1000

    reorg/renaming

--- app/client/main.js
+++ app/client/main.js
@@ -4,5 +4,5 @@ import '/imports/api/databet_collections';
 import '/imports/ui/layouts';
 import '/imports/ui/pages';
 import '/imports/ui/global_helpers';
-import '/imports/ui/login_tools';
+import '/imports/ui/login_logout_templates';
 import '/imports/ui/stylesheets/style.css';
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -1,14 +1,14 @@
 
-import './home-page.html';
 
-import './FAQ.html';
-import './FAQ.js';
+import './faq/FAQ.html';
+import './faq/FAQ.js';
 
-import './InstructorView.html';
-import './InstructorView.js';
+import './front_page/front_page.html';
+import './front_page/InstructorView.html';
+import './front_page/InstructorView.js';
 
-import './InstructorViewArchived.html';
-import './InstructorViewArchived.js';
+import './archive_page/InstructorViewArchived.html';
+import './archive_page/InstructorViewArchived.js';
 
-import './admin/AdminUserSelect.html';
-import './admin/AdminUserSelect.js';
+import './admin_user_select/AdminUserSelect.html';
+import './admin_user_select/AdminUserSelect.js';
