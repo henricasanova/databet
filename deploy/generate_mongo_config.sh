6e9e7b89a47330cdcd4106bf1732dbf6b0474957
henricasanova <henric@hawaii.edu>
2017-01-19 23:02:12 -1000

    More Mongo config

--- deploy/generate_mongo_config.sh
+++ deploy/generate_mongo_config.sh
@@ -9,19 +9,26 @@ fi
 SERVICE_NAME=$1
 PORT=$2
 
-ORIGINAL_CONFIG_FILE=$HOME/mongod.conf
+ORIGINAL_CONFIG_FILE=/etc/mongod.conf
 CONFIG_FILE=/etc/mongo_$SERVICE_NAME.conf
-
 
 echo "Creating $CONFIG_FILE..."
 cat $ORIGINAL_CONFIG_FILE | \
 	sed "s|/var/log/mongodb/mongod.log|/var/log/mongodb/mongod_$SERVICE_NAME.log|" | \
 	sed "s|dbPath: /var/lib/mongo|dbPath: /var/lib/mongo_$SERVICE_NAME|" | \
 	sed "s|pidFilePath: /var/run/mongodb/mongod.pid|pidFilePath: /var/run/mongodb/mongod_$SERVICE_NAME.pid|" | \
-	sed "s|port:.*|port: $PORT|" > /tmp/mongod.conf
+	sed "s|port:.*|port: $PORT|" \
+	> /tmp/mongod.conf
 sudo mv -f /tmp/mongod.conf $CONFIG_FILE
 
 echo "Creating /var/lib/mongo_$SERVICE_NAME directory..."
 sudo mkdir -p /var/lib/mongo_$SERVICE_NAME
 
+ORIGINAL_INITD_FILE=/etc/init.d/mongod 
+INITD_FILE=/etc/init.d/mongod_$SERVICE_NAME
 
+cat $ORIGINAL_INITD_FILE | \
+	sed "s|config: /etc/mongod.conf|config: $CONFIG_FILE|" | \
+	sed "s|CONFIGFILE=\"/etc/mongod.conf\"|CONFIGFILE=\"$CONFIG_FILE\"|" \
+	> /tmp/mongod
+sudo mv -f /tmp/mongod $INITD_FILE
